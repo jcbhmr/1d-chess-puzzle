e604dbff13755b7d93b70da52e965f3ae170f2f6
jcbhmr <jcbhmr@outlook.com>
2024-03-04 15:50:12 -0600

    save

--- next.config.js
+++ next.config.js
@@ -17,6 +17,30 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/1dchess",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default config;
