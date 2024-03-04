d9433462cc3f577269b29cb05ed03c7092e4d429
jcbhmr <jcbhmr@outlook.com>
2024-03-04 15:47:36 -0600

    sasve

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
 
-import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={`font-sans ${inter.variable}`}>
@@ -17,4 +16,4 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   );
 };
 
-export default api.withTRPC(MyApp);
+export default MyApp;
