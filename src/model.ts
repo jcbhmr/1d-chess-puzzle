c2c5c74c812007ff4b3d7af4d1ce3064451b09b1
Jacob Hummer <jcbhmr@outlook.com>
2024-03-13 16:22:11 +0000

    save

--- src/model.ts
+++ src/model.ts
@@ -31,8 +31,9 @@
  * "node" in the game's hierarchy has the opportunity to listen for different
  * game events like `"capture"` and `"move"`.
  *
- * ℹ We extend the `EventTarget` class to add event bubbling for non-native
- * targets.
+ * ℹ We extend the `EventTarget` class to add event bubbling support for non-DOM
+ * nodes. The {@link EventTargetWithBubbling} class uses the `.parentNode`
+ * getter to dispatch events up the hierarchy.
  *
  * Need a refresher on the rules?
  *
@@ -55,187 +56,113 @@
  * @file
  */
 
-class EventTargetWithBubbling extends EventTarget {
-    dispatchEvent(event: Event) {
-        return super.dispatchEvent(event) || ((this as EventTarget & { parentNode?: EventTarget }).parentNode?.dispatchEvent(event) ?? false)
+import assert from "node:assert"
+
+
+
+export class AstrianChessColor {
+    static #key = Symbol("#key");
+    #color: "white" | "black";
+    constructor(k: symbol, color: "white" | "black") {
+        assert(k === AstrianChessColor.#key);
+        assert(color === "white" || color === "black");
+        this.#color = color;
     }
-  }
-  
-
-export type $1DChessColor = "white" | "black"
-
-export class $1DChessGame extends EventTarget {
-    playerWhite = new $1DChessPlayer("white", this)
-    playerBlack = new $1DChessPlayer("black", this)
-    currentPlayer = this.playerWhite;
-    board = new $1DChessBoard(this)
-    constructor() {
-        super()
+    static WHITE = new AstrianChessColor(AstrianChessColor.#key, "white");
+    static BLACK = new AstrianChessColor(AstrianChessColor.#key, "black");
+    get color() {
+        return this.#color;
     }
-
-    get nextPlayer() {
-        if (this.currentPlayer === this.playerWhite) {
-            return this.playerBlack
+    get isWhite() {
+        return this.#color === "white";
+    }
+    get isBlack() {
+        return this.#color === "black";
+    }
+    get opposite() {
+        if (this.isWhite) {
+            return AstrianChessColor.BLACK;
         } else {
-            return this.playerWhite
+            return AstrianChessColor.WHITE;
         }
+    }
+    toString() {
+        return this.#color;
     }
 }
 
-export class $1DChessBoard extends EventTarget {
-    game: $1DChessGame
-    pieces: $1DChessPiece[]
-    layout: ($1DChessPiece | null)[]
-    constructor(game: $1DChessGame) {
-        super()
-        this.game = game
-        this.pieces = [
-            new $1DChessPiece("king", this.game.playerWhite),
-            new $1DChessPiece("knight", this.game.playerWhite),
-            new $1DChessPiece("rook", this.game.playerWhite),
-            new $1DChessPiece("king", this.game.playerBlack),
-            new $1DChessPiece("knight", this.game.playerBlack),
-            new $1DChessPiece("rook", this.game.playerBlack),
-        ]
-        this.layout = [
-            this.pieces[0],
-            this.pieces[1],
-            this.pieces[2],
-            null,
-            null,
-            this.pieces[3],
-            this.pieces[4],
-            this.pieces[5],
-        ]
+export class AstrianChessPosition {
+    #n: number;
+    constructor(position: number) {
+        assert(0 < position && position <= 8);
+        this.#n = position;
     }
-
-    at(position: $1DChessPosition) {
-        return this.layout[position - 1]
+    add(other: AstrianChessPosition) {
+        return new AstrianChessPosition(this.#n + +other);
     }
-
-    getCaptured() {
-        return this.pieces.filter(piece => piece.captured)
+    sub(delta: AstrianChessPosition) {
+        return new AstrianChessPosition(this.#n - +delta);
     }
-
-    get parentNode() {
-        return this.game
+    valueOf() {
+        return this.#n;
+    }
+    toString() {
+        return this.#n.toString();
     }
 }
 
-export class $1DChessPlayer extends EventTarget {
-    game: $1DChessGame
-    color: $1DChessColor
-    constructor(color: $1DChessColor, game: $1DChessGame) {
-        super()
-        this.game = game
-        this.color = color
+export class AstrianChessMove {
+    #start: AstrianChessPosition;
+    #end: AstrianChessPosition;
+    constructor(start: AstrianChessPosition, end: AstrianChessPosition) {
+        this.#start = start;
+        this.#end = end;
     }
-
-    get board() {
-        return this.game.board
+    get start() {
+        return this.#start;
     }
-
-    getPieces() {
-        return this.game.board.pieces.filter(piece => piece.owner === this)
+    get end() {
+        return this.#end;
     }
-
-    movePiece(piece: $1DChessPiece, newPosition: $1DChessPosition) {
-        if (this.game.currentPlayer !== this) {
-            throw new DOMException("It's not your turn", "InvalidStateError")
-        }
-        if (piece.owner !== this) {
-            throw new DOMException("You don't own that piece", "HierarchyRequestError")
-        }
-        if (!piece.position) {
-            throw new DOMException("That piece is captured", "InvalidStateError")
-        }
-        
-        const moves = piece.getLegalMoves()!
-        if (!moves.includes(newPosition)) {
-            throw new DOMException("Invalid move", "InvalidStateError")
-        }
-        
-        const target = this.board.at(newPosition)
-        if (target) {
-            target.dispatchEvent(new Event("capture", { bubbles: true }))
-            target.dispatchEvent(new Event("capture", { bubbles: true }))
-        }
-        this.board.layout[newPosition - 1] = piece
-        this.board.layout[piece.position - 1] = null
-        piece.dispatchEvent(new Event("move"))
-
-        this.game.currentPlayer = this.game.nextPlayer
-        this.game.dispatchEvent(new Event("turn"))
-    }
-
-    get parentNode() {
-        return this.game
+    get delta() {
+        return this.#end.sub(this.#start);
     }
 }
 
-export type $1DChessPieceType = "king" | "knight" | "rook"
-
-export type $1DChessPosition = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
-
-export class $1DChessPiece extends EventTarget {
-    owner: $1DChessPlayer
-    type: $1DChessPieceType
-    constructor(type: $1DChessPieceType, owner: $1DChessPlayer) {
-        super()
-        this.type = type
-        this.owner = owner
+export abstract class AstrianChessPiece extends EventTargetWithBubbling {
+    #color: AstrianChessColor;
+    #position: AstrianChessPosition;
+    constructor(color: AstrianChessColor, position: AstrianChessPosition, owner: AstrianChessColor) {
+        super();
+        this.#color = color;
+        this.#position = position;
     }
-
-    get game() {
-        return this.owner.game
+    get color() {
+        return this.#color;
     }
-
-    get board() {
-        return this.owner.board
-    }
-
     get position() {
-        return this.board.layout.indexOf(this) + 1 || null
+        return this.#position;
     }
-
-    get captured() {
-        return !!this.position
+    set position(position: AstrianChessPosition | number) {
+        this.#position = typeof position === "number" ? new AstrianChessPosition(position) : position;
     }
-
-    getMoves() {
-        if (!this.position) {
-            return null
-        }
-        const moves: $1DChessPosition[] = []
-        switch (this.type) {
-            case "king":
-                if (this.position > 1) {
-                    moves.push(this.position - 1 as $1DChessPosition)
-                }
-                if (this.position < 8) {
-                    moves.push(this.position + 1 as $1DChessPosition)
-                }
-                break
-            case "knight":
-                if (this.position > 2) {
-                    moves.push(this.position - 2 as $1DChessPosition)
-                }
-                if (this.position < 7) {
-                    moves.push(this.position + 2 as $1DChessPosition)
-                }
-                break
-            case "rook":
-                for (let i = 1; i <= 8; i++) {
-                    if (i !== this.position) {
-                        moves.push((i - this.position) as $1DChessPosition)
-                    }
-                }
-                break
-        }
-        return moves
+    abstract getMovements(): AstrianChessMove[];
+    possibleMoves(board: AstrianChessBoard) {
+        return this.getMovements().filter((move) => {
+            const end = move.end;
+            return board.isInBounds(end) && !board.getPiece(end);
+        });
     }
+    legalMoves(board: AstrianChessBoard) {
+        return this.possibleMoves(board);
+    }
+}
 
-    get parentNode() {
-        return this.owner
+export class AstrianChessKing extends AstrianChessPiece {
+    getMovements() {
+        return [
+            new AstrianChessMove(this.position, this.position.add(new AstrianChessPosition(-1))),
+            new AstrianChessMove(this.position, this.position.add(new AstrianChessPosition(1))),
+        ];
     }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import node4web from "vite-plugin-node4web"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), node4web()],
 })
