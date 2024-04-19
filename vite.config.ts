4cc149a5921820a6ba6b01ff90e6ded3aea3564a
Jacob Hummer <jcbhmr@outlook.com>
2024-04-19 14:24:40 -0500

    save

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url"
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.BASE_PATH,
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react()],
   // https://ui.shadcn.com/docs/installation/vite#update-viteconfigts
   resolve: {
     alias: {
